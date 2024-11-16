5b0a507f6cf2475f712cfa30da00301936571b02
Matthew Rowser <33101592+mrowser6754@users.noreply.github.com>
2024-11-16 00:38:50 -0600

    Gravity no longer affecting Player Dash state

--- player/states/motion/dash.gd
+++ player/states/motion/dash.gd
@@ -8,32 +8,31 @@ var _friction: float = 5.0
 
 #region State Functions
 static func _get_state_name() -> String:
-    return "player_dash"
+	return "player_dash"
 
 ## Initialize the state
 func _enter() -> void:
-    self.state_name = Player_StateDash._get_state_name()
-    self.timer.start()
-    self.player.sophia_skin.fall()
+	self.state_name = Player_StateDash._get_state_name()
+	self.timer.start()
+	self.player.sophia_skin.fall()
 
-    self._dash_velocity = self.player.dash_velocity
-    self._dash_velocity = Player_StateMotion.apply_force(self.player.dash_velocity, self.player.movement_velocity.normalized(),
-                                                        1.0, self.player.dash_force)
+	self._dash_velocity = self.player.dash_velocity
+	self._dash_velocity = Player_StateMotion.apply_force(self.player.dash_velocity, self.player.movement_velocity.normalized(), 1.0, self.player.dash_force)
 
 ## Clean up the state
 func _exit() -> void:
-    self.timer.stop()
+	self.timer.stop()
 
 func _handle_input(_event: InputEvent) -> void:
-    pass
+	pass
 
 func _update(_delta: float) -> void:
-    self.player._applied_gravity = 0.0
-    self.player._gravity_velocity = Vector3.ZERO
-    self.player.jump_velocity = Vector3.ZERO
-    self._dash_velocity = Player._handle_velocity_deceleration(self._dash_velocity, self._friction, _delta)
-    self.player.dash_velocity = self._dash_velocity
+	self.player._applied_gravity = 0.0
+	self.player._gravity_velocity = Vector3.ZERO
+	self.player.jump_velocity = Vector3.ZERO
+	self._dash_velocity = Player._handle_velocity_deceleration(self._dash_velocity, self._friction, _delta)
+	self.player.dash_velocity = self._dash_velocity
 #endregion
 
 func _on_timer_timeout() -> void:
-    self.finished.emit(StateMachine.STATE_NAME_PREVIOUS if self.player.is_on_floor() else Player_StateFall._get_state_name())
+	self.finished.emit(StateMachine.STATE_NAME_PREVIOUS if self.player.is_on_floor() else Player_StateFall._get_state_name())
